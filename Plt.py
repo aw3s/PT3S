4367396175cadf1e9bc0e73ea3250ac213638ed5
aw3s <andreashwolters@gmail.com>
2018-03-14 13:15:17 +0100

    .

---
+++ Plt.py
@@ -0,0 +1,140 @@
+"""
+
+---------------------------
+DOCTEST
+---------------------------
+>>> # ---
+>>> # Imports
+>>> # ---
+>>> import logging
+>>> logger = logging.getLogger('PT3S.Mx')  
+>>> import os
+>>> import zipfile
+>>> import pandas as pd
+>>> path = os.path.dirname(__file__)
+>>> # ---
+>>> # Init
+>>> # ---
+
+"""
+
+
+import os
+import sys
+import logging
+logger = logging.getLogger('PT3S.Plt')  
+import argparse
+import unittest
+import doctest
+
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+from nbconvert.preprocessors.execute import CellExecutionError
+
+import timeit
+
+import xml.etree.ElementTree as ET
+import re
+import struct
+import collections
+import zipfile
+import pandas as pd
+import h5py
+
+import subprocess
+
+import warnings
+import tables
+import math
+
+class PltError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+class Plt():
+    """
+      
+    """
+    def __init__(self): 
+        """
+          
+        """
+        logStr = "{0:s}.{1:s}: ".format(self.__class__.__name__, sys._getframe().f_code.co_name)
+        logger.debug("{0:s}{1:s}".format(logStr,'Start.')) 
+        
+        try: 
+            pass
+
+            
+                             
+        except PltError:
+            raise            
+        except Exception as e:
+            logStrFinal="{:s}Exception: Line: {:d}: {!s:s}: {:s}".format(logStr,sys.exc_info()[-1].tb_lineno,type(e),str(e))
+            logger.error(logStrFinal) 
+            raise MxError(logStrFinal)                       
+        finally:
+            logger.debug("{0:s}{1:s}".format(logStr,'_Done.'))     
+
+
+
+if __name__ == "__main__":
+    """
+    Run the Stuff or/and perform Unittests.
+    """
+
+    try:              
+        # Logfile
+        logFileName = 'PT3S.log' 
+        
+        loglevel = logging.INFO
+        logging.basicConfig(filename=logFileName
+                            ,filemode='w'
+                            ,level=loglevel
+                            ,format="%(asctime)s ; %(name)-60s ; %(levelname)-7s ; %(message)s")    
+
+        fileHandler = logging.FileHandler(logFileName)        
+        logger.addHandler(fileHandler)
+
+        consoleHandler = logging.StreamHandler()
+        consoleHandler.setFormatter(logging.Formatter("%(levelname)-7s ; %(message)s"))
+        consoleHandler.setLevel(logging.INFO)
+        logger.addHandler(consoleHandler)
+
+        logStr = "{0:s}.{1:s}: ".format(__name__, sys._getframe().f_code.co_name)
+                                      
+        # Arguments      
+        parser = argparse.ArgumentParser(description='Run the Stuff or/and perform Unittests.'
+        ,epilog='''
+        UsageExample: -v       
+        '''                                 
+        )
+
+        group = parser.add_mutually_exclusive_group()                                
+        group.add_argument("-v","--verbose", help="Debug Messages On", action="store_true",default=True)      
+        group.add_argument("-q","--quiet", help="Debug Messages Off", action="store_true")                  
+        args = parser.parse_args()
+
+        if args.verbose:           
+            logger.setLevel(logging.DEBUG)     
+        else:            
+            logger.setLevel(logging.ERROR)  
+                      
+        logger.debug("{0:s}{1:s}{2:s}".format(logStr,'Start. Argumente:',str(sys.argv))) 
+
+        suite=doctest.DocTestSuite()   
+        unittest.TextTestRunner().run(suite)         
+
+    except SystemExit:
+        pass                                              
+    except:
+        logger.error("{0:s}{1:s}".format(logStr,'logging.exception!')) 
+        logging.exception('')  
+    else:
+        logger.debug("{0:s}{1:s}".format(logStr,'No Exception.')) 
+        sys.exit(0)
+    finally:
+        logger.debug("{0:s}{1:s}".format(logStr,'_Done.')) 
+        sys.exit(0)
