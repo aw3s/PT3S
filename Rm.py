3fd9e71081ed71b93c6193b6d04578eaa72a2f07
aw3s <andreashwolters@gmail.com>
2018-03-15 17:40:42 +0100

    .

--- Rm.py
+++ Rm.py
@@ -10,6 +10,7 @@ DOCTEST
 >>> logger = logging.getLogger('PT3S.Rm')  
 >>> import os
 >>> import pandas as pd
+>>> import matplotlib.pyplot as plt
 >>> path = os.path.dirname(__file__)
 >>> # ---
 >>> # LocalHeatingNetwork
@@ -21,15 +22,21 @@ DOCTEST
 >>> mx.setResultsToMxsFile(NewH5Vec=True)
 >>> xm.Mx(mx=mx)
 >>> rm=Rm(xm=xm,mx=mx)
->>> timeDeltaToT=mx.df.index[1]-mx.df.index[0]
->>> rm.pltNetDHUS(timeDeltaToT=timeDeltaToT)
->>> import matplotlib.pyplot as plt
 >>> plt.close('all')
 >>> fig=plt.figure(
 ...  frameon=True
 ... ,linewidth=1.
 ... ,edgecolor='k') # black
->>> 
+>>> timeDeltaToT=mx.df.index[1]-mx.df.index[0]
+>>> rm.pltNetDHUS(timeDeltaToT=timeDeltaToT)
+>>> (wD,fileName)=os.path.split(xm.xmlFile)
+>>> (base,ext)=os.path.splitext(fileName)
+>>> plotFileName=wD+os.path.sep+base+'.'+'pdf'
+>>> if os.path.exists(plotFileName):                        
+...    os.remove(plotFileName)
+>>> plt.savefig(plotFileName,dpi=300)
+>>> os.path.exists(plotFileName)
+True
 >>> # ---
 >>> # Clean Up
 >>> # ---
@@ -39,6 +46,34 @@ DOCTEST
 ...    os.remove(mx.mxsZipFile)
 >>> if os.path.exists(mx.h5FileMxsVecs):                        
 ...   os.remove(mx.h5FileMxsVecs)
+>>> if os.path.exists(plotFileName):                        
+...   os.remove(plotFileName)
+>>> # ---
+>>> # Test
+>>> # ---
+>>> rootDir=r'C:\\3S\Modelle'
+>>> xmlFile=os.path.join(rootDir,'MVV_FWv12(Bericht Version 1.2)_TLN2_Szenarium.XML')
+>>> xm=Xm.Xm(xmlFile=xmlFile)
+>>> (wDir,modelDir,modelName)=xm.getWDirModelDirModelName()
+>>> mx1File=os.path.join(wDir,os.path.join(modelDir,modelName))+'.MX1'            
+>>> mx=Mx.Mx(mx1File=mx1File)
+>>> mx=Mx.Mx(mx1File=mx1File)
+>>> rm=Rm(xm=xm,mx=mx)
+>>> plt.close('all')
+>>> fig=plt.figure(
+...   frameon=True
+...  ,linewidth=1.
+...  ,edgecolor='k') # black
+>>> timeDeltaToT=pd.to_timedelta('8 minutes 5 seconds')
+>>> rm.pltNetDHUS(timeDeltaToT=timeDeltaToT)
+>>> (wD,fileName)=os.path.split(xm.xmlFile)
+>>> (base,ext)=os.path.splitext(fileName)
+>>> plotFileName=wD+os.path.sep+base+'.'+'pdf'
+>>> if os.path.exists(plotFileName):                        
+...    os.remove(plotFileName)
+>>> plt.savefig(plotFileName,dpi=300)
+>>> os.path.exists(plotFileName)
+True
 """
 
 import os
